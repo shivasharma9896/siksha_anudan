bf5d2a1de2011c5d4ab044c1128b5d312b54a0c4
Vinay Vincent <vinayvincent15@gmail.com>
2022-06-28 09:33:25 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Shiksha Anudan',
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
