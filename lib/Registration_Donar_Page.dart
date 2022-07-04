e83738aa839238dd158a7b77cbcef69f8a896f7c
shivasharma9896 <shiva.sharma2021@vitbhopal.ac.in>
2022-07-04 21:35:04 +0530

    Update Registration_Donar_Page.dart

--- lib/Registration_Donar_Page.dart
+++ lib/Registration_Donar_Page.dart
@@ -538,14 +538,21 @@ class _Registration_Donor extends State<Registration_Donor > {
   }
 
   void signUp(String email, String password) async {
-    if (_formKey.currentState!.validate()) {
-      await auth
-          .createUserWithEmailAndPassword(email: email, password: password)
-          .then((value) => {postDetailsToFirestore()})
-          .catchError((e) {
-        Fluttertoast.showToast(msg: e!.message);
-      });
+    try {
+      final credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
     }
+    postDetailsToFirestore();
   }
 
     postDetailsToFirestore() async{
